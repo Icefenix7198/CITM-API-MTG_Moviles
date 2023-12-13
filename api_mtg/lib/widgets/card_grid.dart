de99ba2c1fbbc72d908286eaa3a8607bfe5fbe72
AndyCubico <astartes99@gmail.com>
2023-12-13 09:52:13 +0100

    Minor fix

--- api_mtg/lib/widgets/card_grid.dart
+++ api_mtg/lib/widgets/card_grid.dart
@@ -14,7 +14,20 @@ class CardGrid extends StatefulWidget {
 }
 
 class _CardGridState extends State<CardGrid> {
-  Color cardColor(MtgCard card) {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 600) {
+          return _gridBuilder(3);
+        } else {
+          return _gridBuilder(2);
+        }
+      },
+    );
+  }
+
+  Color _cardColor(MtgCard card) {
     Color cardColor = Colors.yellow;
     switch (card.colors) {
       case "W":
@@ -24,7 +37,7 @@ class _CardGridState extends State<CardGrid> {
         cardColor = Colors.blue;
         break;
       case "B":
-        cardColor = Color.fromARGB(255, 103, 5, 121);
+        cardColor = const Color.fromARGB(255, 103, 5, 121);
         break;
       case "R":
         cardColor = Colors.red;
@@ -36,53 +49,42 @@ class _CardGridState extends State<CardGrid> {
     return cardColor;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return  LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          if (constraints.maxWidth > 600) {
-            return _gridBuilder(3);
-          } else {
-            return _gridBuilder(2);
-          }
-        },
-      );
-  }
-
   GridView _gridBuilder(int axisCount) {
     return GridView.builder(
-    itemCount: widget.cardList.length,
-    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: axisCount,
-    ),
-    padding: const EdgeInsets.all(20),
-    itemBuilder: (context, index) {
-      return Card(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            color: cardColor(widget.cardList[index]),
+      itemCount: widget.cardList.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: axisCount,
+      ),
+      padding: const EdgeInsets.all(20),
+      itemBuilder: (context, index) {
+        return Card(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              color: _cardColor(widget.cardList[index]),
+            ),
+            child: Column(
+              children: [
+                Image(image: NetworkImage(widget.cardList[index].cropImg)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FittedBox(
+                        fit: BoxFit.contain,
+                        child: Text(widget.cardList[index].name)),
+                    FittedBox(
+                        fit: BoxFit.contain,
+                        child: Text(widget.cardList[index].manaCost)),
+                  ],
+                ),
+                FittedBox(
+                    fit: BoxFit.contain,
+                    child: Text(widget.cardList[index].type)),
+              ],
+            ),
           ),
-          child: Column(
-            children: [
-              Image(image: NetworkImage(widget.cardList[index].cropImg)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FittedBox(
-                      fit: BoxFit.contain, child: Text(widget.cardList[index].name)),
-                  FittedBox(
-                      fit: BoxFit.contain,
-                      child: Text(widget.cardList[index].manaCost)),
-                ],
-              ),
-              FittedBox(
-                  fit: BoxFit.contain, child: Text(widget.cardList[index].type)),
-            ],
-          ),
-        ),
-      );
-    },
-  );
+        );
+      },
+    );
   }
 }
