43db7a2e378629d748d39cfcaacbb5a912cdb8ca
SoniaOL <99950351+SoniaOL@users.noreply.github.com>
2023-12-16 01:19:06 +0100

    Cover in progress

--- api_mtg/lib/Screens/cover.dart
+++ api_mtg/lib/Screens/cover.dart
@@ -9,13 +9,65 @@ class PortadaScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       //appBar: AppBar(title: const Text("Initial Screen")),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Go to New Screen"),
-          onPressed: () {
-            Navigator.of(context).pushNamed("/home");
-          },
+      body: Container(
+        color: Colors.black,
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("MAGIC",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold)),
+              Align(
+                alignment: Alignment(0, -1.5),
+                child: Text(
+                  "THE GATHERING",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              EnterButton(),
+              Text(
+                "sign in",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class EnterButton extends StatelessWidget {
+  const EnterButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 140.0,
+      height: 40.0,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          side: const BorderSide(
+            width: 0.5,
+            color: Colors.red,
+          ),
+          shape: const BeveledRectangleBorder(),
+        ),
+        child: const Text(
+          "ENTER",
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed("/home");
+        },
       ),
     );
   }
