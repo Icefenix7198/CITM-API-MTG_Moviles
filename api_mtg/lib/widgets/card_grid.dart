e822a944b523bfae5118b5b05073e208f2f306aa
AndyCubico <astartes99@gmail.com>
2023-12-19 12:11:07 +0100

    Card grid fix bottom overflow

--- api_mtg/lib/widgets/card_grid.dart
+++ api_mtg/lib/widgets/card_grid.dart
@@ -2,7 +2,6 @@ import 'package:api_mtg/Model/card.dart';
 import 'package:flutter/material.dart';
 
 const double cardWidth = 200;
-const double cardHeight = 200;
 const double cardPadding = 20;
 
 class CardGrid extends StatefulWidget {
@@ -56,6 +55,9 @@ class _CardGridState extends State<CardGrid> {
       itemCount: widget.cardList.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: axisCount,
+        mainAxisSpacing: 20.0,
+        crossAxisSpacing: 20.0,
+        childAspectRatio: 1,
       ),
       padding: const EdgeInsets.all(cardPadding),
       itemBuilder: (context, index) {
@@ -67,27 +69,40 @@ class _CardGridState extends State<CardGrid> {
               arguments: widget.cardList[index],
             );
           },
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: cardWidth,
-              height: cardHeight,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(10)),
-                color: _cardColor(widget.cardList[index]),
-              ),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              color: _cardColor(widget.cardList[index]),
+            ),
+            child: SingleChildScrollView(
               child: Column(
                 children: [
-                  Image(image: NetworkImage(widget.cardList[index].cropImg)),
-                  const SizedBox(height: 8),
-                  Text(
-                    widget.cardList[index].name,
-                    overflow: TextOverflow.ellipsis,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5.0),
+                    child: Image(
+                        image: NetworkImage(widget.cardList[index].cropImg)),
                   ),
-                  const SizedBox(height: 5),
-                  Text(
-                    widget.cardList[index].type,
-                    overflow: TextOverflow.ellipsis,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: Text(
+                      widget.cardList[index].name,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: Text(
+                      widget.cardList[index].type,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                      ),
+                    ),
                   ),
                 ],
               ),
