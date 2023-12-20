946306dd74d79afac86cd991dcd747eb0416e5c0
TheimerTR <albertohidalgo268@gmail.com>
2023-12-20 10:08:45 +0100

    User_Deck screen beenig implemented

--- api_mtg/lib/main.dart
+++ api_mtg/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:api_mtg/Screens/card_screen.dart';
 import 'package:api_mtg/Screens/settings_screen.dart';
 import 'package:api_mtg/Screens/social_screen.dart';
+import 'package:api_mtg/Screens/user_deck_screen.dart';
 import 'package:api_mtg/Screens/user_screen.dart';
 import 'package:api_mtg/Screens/home.dart';
 import 'package:api_mtg/Screens/cover.dart';
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
         "/home/social": (context) => const SocialScreen(),
         "/home/api/card": (context) => const CardScreen(),
         "/home/user": (context) => const UserScreen(),
+        "/home/user/deck": (context) => const UserScreenDeck(),
         "/home/settings": (context) => SettingsScreen(),
       },
     );
